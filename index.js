d9cdc5e4f81c3da1419b52a2c2af281915022337
Nathan Barber <nathanbarber@O2.fios-router.home>
2017-07-10 17:34:32 -0400

    Added express.io for websockets instead of ExpressJS + Socket.IO

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require('express.io');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var path = require('path');
